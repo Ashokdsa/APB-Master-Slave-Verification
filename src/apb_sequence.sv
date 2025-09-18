ecdbbc8467c802c216f16e5b4ce6d4cef479bbb3
Ashokdsa <ashokpiusdsa@gmail.com>
2025-09-18 23:45:32 +0530

    UPDATED apb_sequence

--- src/apb_sequence.sv
+++ src/apb_sequence.sv
@@ -1,4 +1,4 @@
-//need to add a queue to not repest values while writing
+//need to add a queue to not repeat values while writing
 class apb_write_read_sequence extends uvm_sequence#(apb_sequence_item);
   `uvm_object_utils(apb_write_read_sequence)
 
@@ -12,7 +12,7 @@ class apb_write_read_sequence extends uvm_sequence#(apb_sequence_item);
     req.randomize() with 
     {
       req.transfer == 1;
-      req.READ_WRITE != $past(req.READ_WRITE);
+      req.READ_WRITE != read_prev;
       if(!READ_WRITE)
         req.apb_read_addr == req.apb_write_addr;
       req.PRESETn == 1;
@@ -21,7 +21,10 @@ class apb_write_read_sequence extends uvm_sequence#(apb_sequence_item);
     wait_for_item_done();
   endfunction
 
+  bit read_prev;
+
   function void post_randomize();
+    read_prev = ~read_prev;
     if(req.READ_WRITE)
       req.apb_write_addr.rand_mode(0);
     else
@@ -42,7 +45,7 @@ class apb_reset_sequence extends apb_write_read_sequence;
     req.randomize() with 
     {
       req.transfer == 1;
-      req.READ_WRITE != $past(req.READ_WRITE);
+      req.READ_WRITE != read_prev;
       if(!READ_WRITE)
         req.apb_read_addr == req.apb_write_addr;
     }
@@ -56,6 +59,7 @@ class apb_read_write_sequence extends apb_write_read_sequence;
 
   function new(string name = "apb_read_write_sequence");
     super.new(name);
+    read_prev = 1;
   endfunction
 
   function void body();
@@ -64,7 +68,7 @@ class apb_read_write_sequence extends apb_write_read_sequence;
     req.randomize() with 
     {
       req.transfer == 1;
-      req.READ_WRITE != $past(req.READ_WRITE);
+      req.READ_WRITE != read_prev;
       if(READ_WRITE)
         req.apb_write_addr == req.apb_read_addr;
       req.PRESETn == 1;
@@ -74,6 +78,7 @@ class apb_read_write_sequence extends apb_write_read_sequence;
   endfunction
 
   function void post_randomize();
+    read_prev = ~read_prev;
     if(!req.READ_WRITE)
       req.apb_read_addr.rand_mode(0);
     else
@@ -94,7 +99,7 @@ class apb_transfer_sequence extends apb_write_read_sequence;
     req.randomize() with 
     {
       req.transfer == 0;
-      req.READ_WRITE != $past(req.READ_WRITE);
+      req.READ_WRITE != read_prev;
       if(!READ_WRITE)
         req.apb_read_addr == req.apb_write_addr;
       req.PRESETn == 1;
@@ -104,11 +109,10 @@ class apb_transfer_sequence extends apb_write_read_sequence;
   endfunction
 endclass
 
-//NEED TO WRITE FROM HERE
-class apb_write_write_sequence extends apb_write_read_sequence;
-  `uvm_object_utils(apb_write_read_sequence)
+class apb_write_sequence extends apb_write_read_sequence;
+  `uvm_object_utils(apb_write_sequence)
 
-  function new(string name = "apb_write_write_sequence");
+  function new(string name = "apb_write_sequence");
     super.new(name);
   endfunction
 
@@ -126,9 +130,111 @@ class apb_write_write_sequence extends apb_write_read_sequence;
   endfunction
 
   function void post_randomize();
-    if(req.READ_WRITE)
-      req.apb_write_addr.rand_mode(0);
-    else
+    req.apb_write_addr.rand_mode(1);
+  endfunction
+endclass
+
+class apb_read_sequence extends apb_write_read_sequence;
+  `uvm_object_utils(apb_read_sequence)
+
+  function new(string name = "apb_read_sequence");
+    super.new(name);
+  endfunction
+
+  function void body();
+    req = apb_sequence_item::type_id::create("read_sequence_item");
+    wait_for_grant();
+    req.randomize() with 
+    {
+      req.transfer == 1;
+      req.READ_WRITE == 0;
+      req.PRESETn == 1;
+    }
+    send_request(req);
+    wait_for_item_done();
+  endfunction
+
+  function void post_randomize();
+    req.apb_write_addr.rand_mode(1);
+  endfunction
+endclass
+
+class apb_same_sequence extends apb_write_read_sequence;
+  `uvm_object_utils(apb_same_sequence)
+
+  function new(string name = "apb_same_sequence");
+    super.new(name);
+  endfunction
+
+  function void body();
+    req = apb_sequence_item::type_id::create("same_sequence_item");
+    wait_for_grant();
+    req.randomize() with 
+    {
+      req.transfer == 1;
+      req.PRESETn == 1;
+    }
+    send_request(req);
+    wait_for_item_done();
+  endfunction
+
+  bit count;
+
+  function void post_randomize();
+    if(count>0)
+    begin
+      count = 0;
+      req.READ_WRITE.rand_mode(1);
       req.apb_write_addr.rand_mode(1);
+      req.apb_read_addr.rand_mode(1);
+    end
+    else
+    begin
+      count++;
+      req.READ_WRITE.rand_mode(0);
+      req.apb_write_addr.rand_mode(0);
+      req.apb_read_addr.rand_mode(0);
+    end
+  endfunction
+endclass
+
+class apb_diff_slave_sequence extends apb_write_read_sequence;
+  `uvm_object_utils(apb_diff_slave_sequence)
+
+  function new(string name = "apb_diff_slave_sequence");
+    super.new(name);
+  endfunction
+
+  function void body();
+    req = apb_sequence_item::type_id::create("diff_slave_sequence_item");
+    wait_for_grant();
+    req.randomize() with 
+    {
+      req.transfer == 1;
+      req.PRESETn == 1;
+      req.apb_write_addr[8] != choice && req.apb_read_addr[8] != choice;
+      if(count)
+        req.apb_write_addr[7:0] == prev && req.apb_read_addr[7:0] == prev;
+    }
+    send_request(req);
+    wait_for_item_done();
+  endfunction
+
+  bit choice,count;
+  bit[7:0] prev;
+
+  function void post_randomize();
+    choice = req.READ_WRITE ? req.apb_write_addr[8] : req.apb_read_addr[8];
+    if(count>0)
+    begin
+      count = 0;
+      req.READ_WRITE.rand_mode(1);
+    end
+    else
+    begin
+      prev = 
+      count++;
+      req.READ_WRITE.rand_mode(0);
+    end
   endfunction
 endclass
