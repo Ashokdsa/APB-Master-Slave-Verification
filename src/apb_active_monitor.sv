c5771cfb9f2eb6a5aa730170adaa6a628478007f
Rakshith S Shetty <111422371+Rakshith2003@users.noreply.github.com>
2025-09-19 15:33:53 +0530

    Update apb_active_monitor.sv

--- src/apb_active_monitor.sv
+++ src/apb_active_monitor.sv
@@ -1,5 +1,6 @@
 class apb_active_monitor extends uvm_monitor;
   virtual apb_inf vif;
+  event act_e;
   uvm_analysis_port #(apb_sequence_item) item_collected_port;
   apb_sequence_item seq_item;
 
@@ -15,13 +16,16 @@ class apb_active_monitor extends uvm_monitor;
     super.build_phase(phase);
     if(!uvm_config_db#(virtual apb_inf)::get(this, "", "vif", vif))
        `uvm_fatal("NOVIF",{"virtual interface must be set for: ",get_full_name(),".vif"});
+    
+    if(!uvm_config_db#(event)::get(this, "", "ev1", act_e))
+       `uvm_fatal("NOVIF",{"virtual interface must be set for: ",get_full_name(),".vif"});
   endfunction
 
   virtual task run_phase(uvm_phase phase);
    forever 
      begin
+       @(act_e);
        @(posedge vif.mon_cb);
-        
         seq_item.transfer = vif.mon_cb.transfer;
         seq_item.PRESETn = vif.mon_cb.PRESETn;
         seq_item.READ_WRITE = vif.mon_cb.READ_WRITE;
@@ -30,7 +34,14 @@ class apb_active_monitor extends uvm_monitor;
         seq_item.apb_write_data = vif.mon_cb.apb_write_data;
        
         item_collected_port.write(seq_item);
-       repeat(2)@(posedge vif.mon_cb);
+       
+//        if(vif.mon_cb.transfer==1 &&(!prev_transf))
+//          repeat(3)@(posedge vif.MON.CLK);
+//        else if(vif.mon_cb.transfer==1&&(prev_transf))
+//          repeat(2)@(posedge vif.MON.CLK);
+//        else if(vif.mon_cb.transfer==0)
+//          @(posedge vif.MON.CLK);
+//        prev_transf=seq.transfer;
     end
   endtask
 endclass
