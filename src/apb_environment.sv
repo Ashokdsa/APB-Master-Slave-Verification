5f8a7c3bff859cd350ede60767e33500a4b3fafd
Ashok D'sa <109778122+Ashokdsa@users.noreply.github.com>
2025-09-19 09:48:25 +0530

    Update apb_environment.sv

--- src/apb_environment.sv
+++ src/apb_environment.sv
@@ -21,7 +21,7 @@ class apb_environment extends uvm_env;
     function void connect_phase(uvm_phase phase);    								   
       active_agent.active_monitor.item_collected_port.connect(scoreboard.item_collected_export_active); 
       passive_agent.passive_monitor.item_collected_port.connect(scoreboard.item_collected_export_passive);    
-      active_agent.active_monitor.item_collected_port.connect(subscriber.act_mon);
+      active_agent.active_monitor.item_collected_port.connect(subscriber.analysis_export);
       passive_agent.passive_monitor.item_collected_port.connect(subscriber.pass_mon);
   	endfunction:connect_phase
 endclass:apb_environment
