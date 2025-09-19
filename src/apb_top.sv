cfb6ed6e8d1459589280530ab19e20d0f1275c94
Ashokdsa <ashokpiusdsa@gmail.com>
2025-09-19 14:49:37 +0530

    RENAMED interface

--- src/apb_top.sv
+++ src/apb_top.sv
@@ -2,13 +2,17 @@
 import uvm_pkg::*;
 `include "interface.sv"
 `include"apb_pkg.svh"
-
+`include "/fetools/work_area/frontend/Batch_10/APB_project/apbtop.v"
 
 module top;
+  import apb_pkg::*;
   bit clk;
 
   always #5 clk = ~clk;
+
   inf vif(clk);
+
+  APB_Protocol(.PCLK(clk),PRESETn,transfer,READ_WRITE,apb_write_paddr,apb_write_data,apb_read_paddr,PSLVERR,apb_read_data_out);
   apb_test test;
   initial begin
     uvm_config_db#(virtual inf)::set(null, "*", "vif", vif);
