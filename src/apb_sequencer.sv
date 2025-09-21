70c5110fe51f731ebd061ff71eecd23a9a7054c9
Srijan S Uppoor <srijanuppoor@gmail.com>
2025-09-21 15:47:33 +0530

    Update apb_sequencer.sv
    Comment lines and method labels

--- src/apb_sequencer.sv
+++ src/apb_sequencer.sv
@@ -1,7 +1,9 @@
+//APB Sequencer class
+
 class apb_sequencer extends uvm_sequencer#(apb_sequence_item);
-  `uvm_component_utils(apb_sequencer)
+  `uvm_component_utils(apb_sequencer)    // Register with the factory
 
   function new(string name = "apb_sequencer",uvm_component parent = null);
     super.new(name,parent);
-  endfunction
-endclass
+  endfunction:new
+endclass:apb_sequencer
