6a11ae5371d3acf24e880df45e9a360c6b9e3193
Srijan S Uppoor <srijanuppoor@gmail.com>
2025-09-21 16:04:38 +0530

    Update apb_top.sv
    Comment lines and method names

--- src/apb_top.sv
+++ src/apb_top.sv
@@ -1,3 +1,5 @@
+// Top-level module for APB UVM testbench
+
 `include "uvm_macros.svh"
 import uvm_pkg::*;
 `include "apb_pkg.svh"
@@ -8,33 +10,38 @@ import apb_pkg::*;
 module top;
   bit clk;
 
-  always #5 clk = ~clk;
+  // Clock generation : 10ns period 
+  always #5 clk = ~clk;      
 
-  apb_inf vif(clk);
+  // Interface instantiation
+  apb_inf vif(clk);     
 
+  // DUT instantiation
   APB_Protocol DUT(.PCLK(clk),.PRESETn(vif.PRESETn),.transfer(vif.transfer),.READ_WRITE(vif.READ_WRITE),.apb_write_paddr(vif.apb_write_paddr),.apb_write_data(vif.apb_write_data),.apb_read_paddr(vif.apb_read_paddr),.PSLVERR(vif.PSLVERR),.apb_read_data_out(vif.apb_read_data_out));
   
   apb_test test;
-  event act_e,pass_e;
+  event act_e,pass_e;    // Event handles for synchronization
   bit change;
   
   initial begin
+    // UVM Configurations setting
     uvm_config_db#(virtual apb_inf)::set(null, "*", "vif", vif);
     uvm_config_db#(event)::set(null, "*", "ev1", act_e);
     uvm_config_db#(event)::set(null, "*", "ev2", pass_e);
+    
     $dumpfile("wave.vcd");
     $dumpvars(0);
-  end
+  end:initial
 
   initial begin
     vif.PRESETn = 0;
     @(posedge clk);
     vif.PRESETn = 1;
     @(posedge clk);
-  end
+  end:initial
 
   initial begin
-    run_test("apb_test");
+    run_test("apb_test");      // Start UVM test
     $finish;
-  end
-endmodule
+  end:initial
+endmodule:top
