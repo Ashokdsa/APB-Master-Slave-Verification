340ba3cfc42dc3cbc5582cf51acaec3db0c01df6
Srijan S Uppoor <srijanuppoor@gmail.com>
2025-09-21 14:40:39 +0530

    Update apb_driver.sv

--- src/apb_driver.sv
+++ src/apb_driver.sv
@@ -8,7 +8,7 @@ class apb_driver extends uvm_driver #(apb_sequence_item);
     
   function new (string name, uvm_component parent);
     super.new(name, parent);
-  endfunction:mew
+  endfunction:new
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
