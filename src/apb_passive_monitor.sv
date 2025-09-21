ac736fc45af6919b28d103a95f9ca8e5beffdd9b
Srijan S Uppoor <srijanuppoor@gmail.com>
2025-09-21 14:57:05 +0530

    Update apb_passive_monitor.sv
    Add comment lines and method names

--- src/apb_passive_monitor.sv
+++ src/apb_passive_monitor.sv
@@ -1,16 +1,19 @@
-class apb_passive_monitor extends uvm_monitor;
-  virtual apb_inf vif;
-  uvm_analysis_port #(apb_sequence_item) item_collected_port;
-  apb_sequence_item seq_item;
-  event pass_e;
+//Passive APB Monitor observes the APB interface signals without driving them.
 
+class apb_passive_monitor extends uvm_monitor;
+  virtual apb_inf vif;      // Virtual interface handle for connecting to DUT signals
+  uvm_analysis_port #(apb_sequence_item) item_collected_port;    // Analysis port
+  apb_sequence_item seq_item;
+  event pass_e;      // Event to trigger monitoring activity
+
+  // Register the component with UVM factory
   `uvm_component_utils(apb_passive_monitor)
 
   function new (string name, uvm_component parent);
     super.new(name, parent);
     seq_item = new();
     item_collected_port = new("item_collected_port", this);
-  endfunction
+  endfunction:new
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
@@ -19,7 +22,7 @@ class apb_passive_monitor extends uvm_monitor;
     
     if(!uvm_config_db#(event)::get(this, "", "ev2", pass_e))
        `uvm_fatal("NOVIF",{"virtual interface must be set for: ",get_full_name(),".vif"});
-  endfunction
+  endfunction:build_phase
 
   virtual task run_phase(uvm_phase phase);
    forever 
@@ -32,6 +35,6 @@ class apb_passive_monitor extends uvm_monitor;
         if(get_report_verbosity_level() >= UVM_MEDIUM)
           $display("READ_DATA_OUT = %0d\tPSLVERR = %0b",seq_item.apb_read_data_out,seq_item.PSLVERR);
     end
-  endtask
-endclass
+  endtask:run_phase
+endclass:apb_passive_monitor
     
